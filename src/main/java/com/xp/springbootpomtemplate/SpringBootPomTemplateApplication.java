01231dbbaf6abf47f83876218863b288fce7a8dd
51ite.com <793379415@qq.com>
2019-07-02 17:30:31 +0800

    first commit

---
+++ src/main/java/com/xp/springbootpomtemplate/SpringBootPomTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.xp.springbootpomtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPomTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPomTemplateApplication.class, args);
+    }
+
+}
